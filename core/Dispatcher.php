23a49dd30270b2ca13da126c2d0b39ee214d65da
stjournaud <stjournaud@gmail.com>
2020-09-08 13:00:49 +0200

    creation dispatcher &  includes

---
+++ core/Dispatcher.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Dispatcher
+* Permet de charger le controller en fonction de la requête utilisateur
+**/
+class Dispatcher{
+
+    var $request;   // Object Request
+    
+    /**
+	* Fonction principale du dispatcher
+	* Charge le controller en fonction du routing
+	**/
+    function __construct() {
+        
+    }
+}
---
+++ core/includes.php
@@ -0,0 +1,2 @@
+<?php
+require 'Dispatcher.php';
