2f46e04a247857305cde60b52371a5ff3e423c62
stjournaud <stjournaud@gmail.com>
2020-09-08 15:08:40 +0200

    prefix

--- core/Request.php
+++ core/Request.php
@@ -2,6 +2,9 @@
 class Request{
 
     public $url;                // URL appellée par l'utilisateur
+    public $page = 1;           // pour la pagination 
+    public $prefix = false;     // Prefixage des urls /prefix/url
+    public $data = false;       // Données envoyé dans le formulaire
 
     function __construct() {
         $this->url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
